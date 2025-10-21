a697003c11d5401df471e9f9f644acf60968e8bd
GoldPigg <Gold_Pigg@outlook.com>
2025-10-22 00:06:33 +0800

    refactor: split Mason lsp config into 'lua/lsp'

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -13,6 +13,7 @@ return {
     'saghen/blink.cmp',
   },
   config = function()
+    local Util = require 'util'
     -- LSP servers and clients are able to communicate to each other what features they support.
     --  By default, Neovim doesn't support everything that is in the LSP specification.
     --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
@@ -41,22 +42,17 @@ return {
       -- But for many setups, the LSP (`ts_ls`) will work just fine
       -- ts_ls = {},
       --
-
-      lua_ls = {
-        -- cmd = { ... },
-        -- filetypes = { ... },
-        -- capabilities = {},
-        settings = {
-          Lua = {
-            completion = {
-              callSnippet = 'Replace',
-            },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
-          },
-        },
-      },
     }
+    Util.deep_lsmod('lsp', function(name, path, type)
+      if type == 'file' or type == 'link' then
+        local mod, err = loadfile(path)
+        if mod == nil then
+          vim.print('When load ' .. name .. ' from ' .. path .. ' error\n' .. err)
+        else
+          servers = vim.tbl_extend('force', servers, mod())
+        end
+      end
+    end)
 
     -- Ensure the servers and tools above are installed
     --
@@ -72,9 +68,6 @@ return {
     -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
-    })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     require('mason-lspconfig').setup {
