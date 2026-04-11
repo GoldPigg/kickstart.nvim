c855f5112cab1d15e5fabb20b922f56d3e65d6f6
GoldPigg <Gold_Pigg@outlook.com>
2026-04-12 00:42:30 +0800

    fix(mason.lua): use new API to configure lsp

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -13,7 +13,6 @@ return {
   },
   config = function()
     local Util = require 'util'
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
 
     local servers = {}
 
@@ -40,19 +39,10 @@ return {
     end
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-    require('mason-lspconfig').setup {
-      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-      automatic_installation = false,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for ts_ls)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
+    vim.print(servers)
+    for name, server in pairs(servers) do
+      vim.lsp.config(name, server)
+      vim.lsp.enable(name)
+    end
   end,
 }
